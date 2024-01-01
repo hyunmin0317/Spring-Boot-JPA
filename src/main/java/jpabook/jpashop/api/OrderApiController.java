5361047cbf08da9f3d7ac67b82685bdec81fdbdb
hyunmin0317 <choihm9903@naver.com>
2024-01-01 15:14:18 +0900

    feat: OrderApiController ordersV3 작성

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -24,4 +24,12 @@ public class OrderApiController {
                 .map(OrderDto::new)
                 .collect(toList());
     }
+
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        return orders.stream()
+                .map(OrderDto::new)
+                .collect(toList());
+    }
 }
