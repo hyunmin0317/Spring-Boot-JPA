d2d2b8dbb2a080e6528a70bf29ccb296d1825e42
hyunmin0317 <choihm9903@naver.com>
2024-03-02 14:16:32 +0900

    feat: Order 엔티티 작성 (#2)

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
