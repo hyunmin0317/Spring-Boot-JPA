54cca071f0186e5f490f52188cc30ceea6b7c3d5
hyunmin0317 <choihm9903@naver.com>
2024-03-04 21:29:31 +0900

    feat: 주문 서비스 개발 - 주문 취소 (#6)

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -49,9 +49,19 @@ public class OrderService {
     /**
      * 주문 취소
      */
-
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        //주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        //주문 취소
+        order.cancel();
+    }
 
     /**
      * 주문 검색
      */
+    //TODO 주문 검색 개발
+//    public List<Order> findOrders(OrderSearch orderSearch) {
+//        return orderRepository.findAll(orderSearch);
+//    }
 }
