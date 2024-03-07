87ca2126aa06e17cefd8dbae93449095ee570d1c
hyunmin0317 <choihm9903@naver.com>
2024-03-07 21:24:17 +0900

    feat: saveMemberV1 API 개발 (#8)

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.api;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.dto.CreateMemberResponse;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+}
