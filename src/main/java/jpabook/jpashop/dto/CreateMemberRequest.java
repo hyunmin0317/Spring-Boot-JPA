9b6cd669f802191d1207a07e9abeb039eb471541
hyunmin0317 <choihm9903@naver.com>
2024-03-07 21:29:44 +0900

    refactor: validation 추가 (#8)

--- src/main/java/jpabook/jpashop/dto/CreateMemberRequest.java
+++ src/main/java/jpabook/jpashop/dto/CreateMemberRequest.java
@@ -1,9 +1,11 @@
 package jpabook.jpashop.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class CreateMemberRequest {
 
+    @NotEmpty
     private String name;
 }
